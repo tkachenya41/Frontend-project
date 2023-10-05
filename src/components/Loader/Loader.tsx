a124920684b330dc3df013fe72fcd33439bd79db
tkachenya41 <tkachenya41@gmail.com>
2023-10-05 16:57:25 +0300

    fix: add custom loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,19 +1,17 @@
 'use client';
 
-import Image from 'next/image';
-import Styles from '@/app/page.module.scss';
+import Styles from './Loader.module.scss';
 
 export default function Loader({ isLoading }: { isLoading: boolean }) {
   return (
     <>
       {isLoading && (
-        <Image
-          width={300}
-          height={300}
-          src='/loading.gif'
-          alt='Loading'
-          className={Styles.spinner}
-        />
+        <div className={Styles.loader}>
+          <div className={Styles.loader__square}></div>
+          <div className={Styles.loader__square}></div>
+          <div className={Styles.loader__square}></div>
+          <div className={Styles.loader__square}></div>
+        </div>
       )}
     </>
   );
